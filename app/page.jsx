d34c78545539a421fc17617e5157c0dd62bc64c4
Andrea Piano <andreapiano@Andreas-MacBook-Pro.local>
2023-04-10 16:22:50 +0200

    feat: both pages are being fetched

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,25 @@
 import Movie from "./Movie";
 
 export default async function Home() {
-  const data = await fetch(
+  const page1Data = await fetch(
     `https://api.themoviedb.org/4/list/8248147?page=1&api_key=${process.env.API_KEY}&language=en-US&page=1`
   );
+  const page2Data = await fetch(
+    `https://api.themoviedb.org/4/list/8248147?page=1&api_key=${process.env.API_KEY}&language=en-US&page=2`
+  );
 
-  const res = await data.json();
-  console.log(res);
+  const page1Res = await page1Data.json();
+  const page2Res = await page2Data.json();
+
+  const results = [...page1Res.results, ...page2Res.results];
+
+  console.log(results);
+
   return (
     <main className="my-8">
       <div className="grid gap-16 grid-cols-fluid">
         {" "}
-        {res?.results?.map((movie) => (
+        {results?.map((movie) => (
           <Movie
             key={movie?.id}
             id={movie?.id}
