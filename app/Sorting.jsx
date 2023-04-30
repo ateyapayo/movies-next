99056b7e514fc86f824e8cf4c2f8536b8688e219
Andrea Piano <andreapiano@Andreas-MacBook-Pro.local>
2023-04-30 16:49:53 +0200

    feat: dropdown tailwind sort by

--- app/Sorting.jsx
+++ app/Sorting.jsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 export default function Sorting({
   content,
   setContent,
@@ -18,6 +24,8 @@ export default function Sorting({
     );
     setContent(sortedResults);
     setterSortPopularity(getterSortPopularity === "desc" ? "asc" : "desc");
+    setterSortAlphabet("asc");
+    setterSortVote("desc");
   };
 
   const orderByVote = () => {
@@ -28,6 +36,8 @@ export default function Sorting({
     );
     setContent(sortedResults);
     setterSortVote(getterSortVote === "desc" ? "asc" : "desc");
+    setterSortPopularity("desc");
+    setterSortAlphabet("asc");
   };
 
   const orderByAlphabet = () => {
@@ -43,30 +53,88 @@ export default function Sorting({
     });
     setContent(sortedResults);
     setterSortAlphabet(getterSortAlphabet === "desc" ? "asc" : "desc");
+    setterSortPopularity("desc");
+    setterSortVote("desc");
   };
 
   return (
     <>
       {content.length > 0 && (
-        <div>
+        <Menu as="div" className="relative inline-block text-left">
           <div>
-            <button onClick={orderByPopularity}>
-              <h1>Order by Popularity</h1>
-            </button>
+            <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+              Options
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5 ml-2 -mr-1"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </Menu.Button>
           </div>
 
-          <div>
-            <button onClick={orderByVote}>
-              <h1>Order by Vote</h1>
-            </button>
-          </div>
-
-          <div>
-            <button onClick={orderByAlphabet}>
-              <h1>Order Alphabetically</h1>
-            </button>
-          </div>
-        </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="py-1">
+                <Menu.Item onClick={orderByPopularity}>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-sm"
+                      )}
+                    >
+                      Popularity
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item onClick={orderByVote}>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-sm"
+                      )}
+                    >
+                      Vote
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item onClick={orderByAlphabet}>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-sm"
+                      )}
+                    >
+                      A-Z
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
       )}
     </>
   );
