4d435ec744c0b4831e1e8724e1b27ec89232fe5f
Andrea Piano <andreapiano@Andreas-MacBook-Pro.local>
2023-05-01 18:09:48 +0200

    feat: new api 01/05

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,10 @@ import HomeContent from "./HomeContent";
 
 export default async function Home() {
   const page1Data = await fetch(
-    `https://api.themoviedb.org/4/list/8248522?page=1&api_key=${process.env.API_KEY}&language=en-US&page=1`
+    `https://api.themoviedb.org/4/list/8251340?page=1&api_key=${process.env.API_KEY}&language=en-US&page=1`
   );
   const page2Data = await fetch(
-    `https://api.themoviedb.org/4/list/8248522?page=1&api_key=${process.env.API_KEY}&language=en-US&page=2`
+    `https://api.themoviedb.org/4/list/8251340?page=1&api_key=${process.env.API_KEY}&language=en-US&page=2`
   );
 
   const page1Res = await page1Data.json();
