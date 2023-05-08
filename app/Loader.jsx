c2701cd16c25fa0c159482e5b2a0fe11878f67e2
ateyapayo <andrea.piano@softfobia.com>
2023-05-08 10:55:37 +0200

    feat: improved loader

--- app/Loader.jsx
+++ app/Loader.jsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { ClipLoader } from "react-spinners";
 
 export default function Loader({ customSize }) {
+  const pathname = usePathname();
   return (
-    <div className="flex items-center justify-center loader-div">
+    <div
+      className={`flex items-center justify-center h-screen ${
+        pathname == "/" ? "loader-card" : "loader-detail"
+      }`}
+    >
       <ClipLoader color="#E50914" size={customSize} />
     </div>
   );
