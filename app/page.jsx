3f6dfc33643fd8135f93b18210722dfd19a7901d
ateyapayo <andrea.piano@softfobia.com>
2023-05-08 16:52:21 +0200

    feat: catching error on main page file

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,26 @@
 import HomeContent from "./components/HomeContent";
 
 export default async function Home() {
-  const page1Data = await fetch(
-    `https://api.themoviedb.org/4/list/8251340?page=1&api_key=${process?.env?.API_KEY}&language=en-US&page=1`
-  );
-  const page2Data = await fetch(
-    `https://api.themoviedb.org/4/list/8251340?page=1&api_key=${process?.env?.API_KEY}&language=en-US&page=2`
-  );
+  try {
+    const page1Data = await fetch(
+      `https://api.themoviedb.org/4/list/8251340?page=1&api_key=${process?.env?.API_KEY}&language=en-US&page=1`
+    );
+    const page2Data = await fetch(
+      `https://api.themoviedb.org/4/list/8251340?page=1&api_key=${process?.env?.API_KEY}&language=en-US&page=2`
+    );
 
-  const page1Res = await page1Data?.json();
-  const page2Res = await page2Data?.json();
+    if (!page1Data.ok || !page2Data.ok) {
+      throw new Error("Failed to fetch data from API");
+    }
 
-  const results = [...page1Res?.results, ...page2Res?.results];
+    const page1Res = await page1Data.json();
+    const page2Res = await page2Data.json();
 
-  return <HomeContent content={results} />;
+    const results = [...page1Res?.results, ...page2Res?.results];
+
+    return <HomeContent content={results} />;
+  } catch (error) {
+    console.error(error);
+    return <p>Something went wrong. Please try again later.</p>;
+  }
 }
