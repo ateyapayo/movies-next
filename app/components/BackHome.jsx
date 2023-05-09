d6cf04af09711b6635def5f54b12101a42e40514
ateyapayo <andrea.piano@softfobia.com>
2023-05-09 09:39:52 +0200

    feat: event.preventDefault() in link

--- app/components/BackHome.jsx
+++ app/components/BackHome.jsx
@@ -1,23 +1,25 @@
 "use client";
 
-import Link from "next/link";
-
+import { useRouter } from "next/navigation";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useSearchContext } from "@/context/SearchContext";
-import { useRouter } from "next/navigation";
 
 export default function BackHome({}) {
-  const context = useSearchContext();
   const router = useRouter();
+  const context = useSearchContext();
 
-  const pushLink = () => {
-    router?.push("/");
+  const handleLinkClick = (event) => {
+    event.preventDefault();
+    router.push("/");
   };
 
   return (
     <>
       {context?.keyword?.getter && (
-        <div className="mb-4 mr-3 flex div-back" onClick={pushLink}>
+        <div
+          className="mb-4 mr-3 flex div-back pointer"
+          onClick={handleLinkClick}
+        >
           <ArrowBackIcon className="arrow-back" />
           <span className="text-back">Go back to results</span>
         </div>
