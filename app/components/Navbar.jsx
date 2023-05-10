30553d707e6edcd24e81d574c7ecc2572b793d1d
ateyapayo <andrea.piano@softfobia.com>
2023-05-10 10:42:54 +0200

    feat: better css semantic on navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
 
 import NetflixLogo from "../../public/logo.png";
 import { useSearchContext } from "../../context/SearchContext";
@@ -12,9 +11,7 @@ import Search from "./Search";
 export default function Navbar({}) {
   const context = useSearchContext();
 
-  const is404 = context?.custom404?.errorPage;
   const reset = context?.filters?.clear;
-  const pathname = usePathname();
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -24,16 +21,12 @@ export default function Navbar({}) {
   return (
     <>
       <div
-        className={`flex navbar py-1 navbar-scrolled  ${
+        className={`flex navbar py-1 navbar-scrolled ${
           pathname == "/" ? "home-navbar" : "detail-navbar"
         }`}
       >
         <div className="container navbar-container">
-          <Link
-            className={`link-logo mr-10 ${is404 && "error-logo"}`}
-            href="/"
-            onClick={scrollToTop}
-          >
+          <Link className="link-logo mr-10" href="/" onClick={scrollToTop}>
             <Image
               onClick={reset}
               className="logo"
