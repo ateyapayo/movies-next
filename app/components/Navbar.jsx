1b1bfa28c10eaf5905da6c6d88d144eaff2d5b7d
Andrea Piano <andreapiano@Andreas-MacBook-Pro.local>
2023-05-12 21:17:27 +0200

    feat: navbar link-logo as next

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -8,9 +8,11 @@ import NetflixLogo from "../../public/logo.png";
 import { useSearchContext } from "../../context/SearchContext";
 
 import Search from "./Search";
+import { useRouter } from "next/navigation";
 
 export default function Navbar({}) {
   const context = useSearchContext();
+  const router = useRouter();
   const pathname = usePathname();
 
   const reset = context?.filters?.clear;
@@ -28,7 +30,15 @@ export default function Navbar({}) {
         }`}
       >
         <div className="container navbar-container">
-          <Link className="link-logo mr-10" href="/" onClick={scrollToTop}>
+          <Link
+            className="link-logo mr-10"
+            href="/"
+            onClick={
+              pathname.startsWith("/tv") || pathname.startsWith("/movie")
+                ? () => router.back()
+                : scrollToTop
+            }
+          >
             <Image
               onClick={reset}
               className="logo"
