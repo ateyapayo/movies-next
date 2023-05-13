23d3638aa600b7e0ddd777fe01c2362f6e277fec
Andrea Piano <andreapiano@Andreas-MacBook-Pro.local>
2023-05-13 16:01:10 +0200

    feat: search bar shouldn't be zooming javascript fix

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -7,12 +7,17 @@ import { useSearchContext } from "@/context/SearchContext";
 export default function Search({}) {
   const context = useSearchContext();
 
+  const handleTouchStart = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="search-section">
       <div className="bg-transparent search-container">
         <SearchOutlinedIcon className="search-icon" />
         <input
           value={context?.keyword?.getter}
+          onTouchStart={handleTouchStart}
           onChange={(e) => {
             context?.filters?.noSorting();
             context?.keyword?.setter(e.target.value);
